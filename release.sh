c98269444d272577d2288ded8d17524b7e2a4526
Jeroen Venema <jeroen.venema@gmail.com>
2025-04-08 16:30:07 +0200

    VDP v2.13.1 / MOS v2.3.3

--- release.sh
+++ release.sh
@@ -13,5 +13,5 @@ rm ./firmware/input/firmware.bin
 wget https://github.com/AgonConsole8/agon-vdp/releases/latest/download/firmware.bin -P ./firmware/input
 pio run --target buildfs
 pio run
-python3 -m esptool --chip esp32 merge_bin -o ./firmware/merged.bin --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 "./.pio/build/esp32dev/bootloader.bin" 0x8000 "./.pio/build/esp32dev/partitions.bin" 0x10000 "./.pio/build/esp32dev/firmware.bin" 0x150000 "./firmware/input/firmware.bin" 0x290000 "./.pio/build/esp32dev/spiffs.bin"
+esptool.py --chip esp32 merge_bin -o ./firmware/merged.bin --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 "./.pio/build/esp32dev/bootloader.bin" 0x8000 "./.pio/build/esp32dev/partitions.bin" 0x10000 "./.pio/build/esp32dev/firmware.bin" 0x150000 "./firmware/input/firmware.bin" 0x290000 "./.pio/build/esp32dev/spiffs.bin"
 
